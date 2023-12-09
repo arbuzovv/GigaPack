bfd2f0a1f3b33e4294fc7c29130b4933b8dd2341
Vyacheslav Arbuzov <arbuzov1989@gmail.com>
2023-12-09 21:24:17 +0500

    Update AlterGiga.R

--- src/AlterGiga.R
+++ src/AlterGiga.R
@@ -1,5 +1,4 @@
 
-
 library(rusquant)
 library(scales)
 library(data.table)
@@ -9,44 +8,49 @@ library(ggplot2)
 all_fields = getSymbolList('gigapack')
 
 #### Compare real price and Alter Price trajectories ####
-real_price = getSymbols.Gigapack('ROSN',field = 'ret')
-alter_price = getSymbols.Gigapack('ROSN',field = 'ret',fake = T,reps = 100,trim = 1)
+real_price = getSymbols.Gigapack('SBER',field = 'ret')
+alter_price = getSymbols.Gigapack('SBER',field = 'ret',fake = T,reps = 30,trim = 1)
 
-plot(100*exp(cumsum(real_price$value)),type='l',ylim = c(30,300),ylab='Price',xlab='Time')
+plot(100*exp(cumsum(real_price$ret)),type='l',ylim = c(30,300),ylab='Price',xlab='Time')
 grid()
-for(i in 4:100) lines(100*exp(cumsum(alter_price[,i,with=FALSE][[1]])),col=alpha('gray',0.3))
+for(i in 4:100) lines(100*exp(cumsum(alter_price[sim==i]$ret)),col=alpha('gray',0.3))
 lines(100*exp(cumsum(real_price$value)),col='black',lwd=3)
 
 ## Compare real price and Alter Price characteristics
-names(alter_price)[4] = 'value'
+real_price$sim = 0
+setcolorder(real_price, names(alter_price))
 price_data = rbind(data.table(real_price,type='real'),data.table(alter_price[,1:4],type='alter'))
-  
+
 #compare density
-ggplot(price_data, aes(value,fill=factor(type),alpha=.01)) + geom_density(adjust=.3)
+ggplot(price_data, aes(ret,fill=factor(type),alpha=.01)) + geom_density(adjust=.3)
 
 #compare ACF
-acf(real_price$value)
-acf(alter_price$value)
+acf(real_price$ret)
+acf(alter_price[sim==1]$ret)
+acf(alter_price[sim==2]$ret)
 
 
 #### Compare AlgoPack data with Alternative Data
 #### Compare real price and Alter Price trajectories ####
-real_data = getSymbols.Gigapack('ROSN',field = 'trades.skew')
+real_data = getSymbols.Gigapack('ROSN',field = 'trades.skew',from='2023-01-01')
 alter_data = getSymbols.Gigapack('ROSN',field = 'trades.skew',fake = T,reps = 30,trim = 0.1)
 
 
-plot(real_data$value,type='l',ylab='disb.mean',xlab='Time')
+
+
+plot(real_data[date>'2023-09-01']$trades.skew,type='l',ylab='trades.skew',xlab='Time')
 grid()
-for(i in 4:30) lines(alter_data[,i,with=FALSE][[1]],col=alpha('gray',0.3))
-lines(real_data$value,col='black',lwd=0.1)
+for(i in 1:10) lines(alter_data[date>'2023-09-01'][sim==i]$trades.skew,col=alpha('gray',0.5))
+lines(real_data[date>'2023-09-01']$trades.skew,col='black',lwd=0.2)
 
 
-names(alter_data)[4] = 'value'
+real_data$sim = 0
+setcolorder(real_data, names(alter_data))
 compare_data = rbind(data.table(real_data,type='real'),data.table(alter_data[,1:4],type='alter'))
 
 #compare density
-ggplot(compare_data, aes(value,fill=factor(type),alpha=.01)) + geom_density(adjust=.3)
+ggplot(compare_data, aes(trades.skew  ,fill=factor(type),alpha=.01)) + geom_density(adjust=.3)
 
 #compare ACF
-acf(real_data$value)
-acf(alter_data$value)
+acf(real_data$trades.skew)
+acf(alter_data[sim==1]$trades.skew)
